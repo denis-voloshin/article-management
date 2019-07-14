5dfc0b35a4b3881d6309c9209528805a708bcfe8
Denis Voloshin <denvol1996@gmail.com>
2019-07-14 14:00:34 +0300

    Added dev and start scripts

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ import morgan from 'morgan';
 
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
