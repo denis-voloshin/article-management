d4133e97abe284ade75df25b714e54ede139ef70
Denis Voloshin <denvol1996@gmail.com>
2019-07-14 23:41:53 +0300

    Created CRUD for articles

--- server/api/controllers/article.js
+++ server/api/controllers/article.js
@@ -1,31 +1,105 @@
+import mongoose from 'mongoose';
+
+import { ArticleModel } from '../models/article';
+
 export const ArticleController = {};
 
-ArticleController.articleGetList = (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling article get list...'
-  });
+ArticleController.articleGetList = async (req, res, next) => {
+  try {
+    const articles = await ArticleModel.find().exec();
+
+    res.status(200).json({ articles });
+  } catch (err) {
+    next(err);
+  }
 };
 
-ArticleController.articleCreate = (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling article create...'
-  });
+ArticleController.articleCreate = async (req, res, next) => {
+  try {
+    const { title, text, isPublic } = req.body;
+
+    const article = new ArticleModel({
+      _id: mongoose.Types.ObjectId(),
+      title,
+      text,
+      isPublic
+    });
+
+    await article.save();
+
+    res.status(201).json({
+      message: 'Article was created',
+      article
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-ArticleController.articleGet = (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling article get...'
-  });
+ArticleController.articleGet = async (req, res, next) => {
+  try {
+    const { articleId } = req.params;
+
+    const article = await ArticleModel.findById(articleId);
+
+    if (!article) {
+      return res.status(404).json({
+        message: 'Article was not found'
+      });
+    }
+
+    res.status(200).json({ article });
+  } catch (err) {
+    next(err);
+  }
 };
 
-ArticleController.articleUpdate = (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling article update...'
-  });
+ArticleController.articleUpdate = async (req, res, next) => {
+  try {
+    const { articleId } = req.params;
+    const { title, text, isPublic } = req.body;
+
+    const article = await ArticleModel.findById(articleId);
+
+    if (!article) {
+      return res.status(404).json({
+        message: 'Article was not found'
+      });
+    }
+
+    await ArticleModel.updateOne({ _id: articleId }, {
+      title,
+      text,
+      isPublic,
+      updatedAt: Date.now()
+    }).exec();
+
+    res.status(200).json({
+      message: 'Article was updated'
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-ArticleController.articleDelete = (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling article delete'
-  });
+ArticleController.articleDelete = async (req, res, next) => {
+  try {
+    const { articleId } = req.params;
+
+    const article = await ArticleModel.findById(articleId);
+
+    if (!article) {
+      return res.status(404).json({
+        message: 'Article was not found'
+      });
+    }
+
+    await ArticleModel.deleteOne({ _id: articleId }).exec();
+
+    res.status(200).json({
+      message: 'Article was deleted'
+    });
+  } catch (err) {
+    next(err);
+  }
 };
---
+++ server/api/models/article.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const articleSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  isPublic: { type: String, required: true },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': null }
+});
+
+export const ArticleModel = mongoose.model('Article', articleSchema);
