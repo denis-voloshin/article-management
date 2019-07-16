be1fdb15cc5c9d38cd0e71195e8c445ed27545a4
Denis Voloshin <denvol1996@gmail.com>
2019-07-16 23:00:33 +0300

    Added more article validations

--- server/api/validators/article.js
+++ server/api/validators/article.js
@@ -1,8 +1,14 @@
 import { body } from 'express-validator';
 
 export const ArticleValidator = [
-  body('title').exists().withMessage('Article title cannot be empty'),
-  body('text').exists().withMessage('Article text cannot be empty'),
+  body('title')
+    .exists().withMessage('Article title cannot be empty')
+    .isString().withMessage('Article title should be a string'),
+
+  body('text')
+    .exists().withMessage('Article text cannot be empty')
+    .isString().withMessage('Article text should be a string'),
+
   body('isPublic')
     .exists().withMessage('Article isPublic flag cannot be empty')
     .isBoolean().withMessage('Article isPublic flag should be a boolean')
