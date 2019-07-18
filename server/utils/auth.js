cd9b782970391938f2e0f74c4bd6bf963bc4698e
Denis Voloshin <denvol1996@gmail.com>
2019-07-18 22:47:06 +0300

    Added isNil authorization header checking

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -9,6 +9,12 @@ export const generateUserToken = (_id, login) => jwt.sign(
   { expiresIn: process.env.JWT_EXPIRES }
 );
 
-export const getTokenFromHeader = R.compose(R.last, R.split('Bearer '));
+export const getTokenFromHeader = authorization => {
+  if (R.isNil(authorization)) {
+    return void 0;
+  }
+
+  return R.compose(R.last, R.split('Bearer '))(authorization);
+};
 
 export const getUserByToken = token => UserModel.findOne({ token }).exec();
